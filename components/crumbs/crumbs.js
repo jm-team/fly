f18c0ff6ad968cd64d536c0e2452cb25ad501983
piluohen <1012106967@qq.com>
2017-01-17 16:45:58 +0800

    增加radio checkbox 基础demo

--- components/crumbs/crumbs.js
+++ components/crumbs/crumbs.js
@@ -3,9 +3,9 @@ $(function () {
     $('.jm-radio-box').on('click', function () {
         if($(this).hasClass('jm-radio-disabled') == false){
             $(this).children('.jm-radio').attr('checked', true);
-            $(this).children('.jm-radio-icon').addClass('active')
+            $(this).children('.jm-radio-icon').addClass('jm-icon-radio').removeClass('jm-icon-radio-none');
             $(this).siblings().children('.jm-radio').removeAttr('checked');
-            $(this).siblings().children('.jm-radio-icon').removeClass('active')
+            $(this).siblings().children('.jm-radio-icon').removeClass('jm-icon-radio').addClass('jm-icon-radio-none')
         } 
     });
     // 多选框事件
@@ -14,10 +14,10 @@ $(function () {
             $(this).addClass('jm-checkbox-disabled')
         } else if ($(this).children('.jm-checkbox').attr('checked')) {
             $(this).children('.jm-checkbox').removeAttr('checked');
-            $(this).children('.jm-checkbox-icon').removeClass('active')
+            $(this).children('.jm-checkbox-icon').addClass('jm-icon-checkbaba').removeClass('jm-icon-checkbook')
         } else {
             $(this).children('.jm-checkbox').attr('checked', true);
-            $(this).children('.jm-checkbox-icon').addClass('active')
+            $(this).children('.jm-checkbox-icon').addClass('jm-icon-checkbook').removeClass('jm-icon-checkbaba')
         }
     })
-})
+});
