94dc3a481722687f8b4a71a27f4532bc32838be4
luonadou <luonadou@sina.com>
2017-02-16 16:37:06 +0800

    alertWarning

--- components/index.js
+++ components/index.js
@@ -4,3 +4,4 @@ export { default as Button } from './button';
 export { default as Radio } from './radio';
 
 export { default as Checkbox } from './checkbox';
+export { default as AlertWarning } from './alertWarning';
