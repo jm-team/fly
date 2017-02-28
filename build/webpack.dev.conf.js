ab5d2e20ab9b90aaa05c8915d33b0e7116b2f555
ICELI <woticeli@gmail.com>
2017-02-28 14:22:19 +0800

    chore: remove fly.js

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -28,7 +28,8 @@ module.exports = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
-      inject: true
+      inject: true,
+      chunks: ['app', 'vendor']
     }),
     new FriendlyErrorsPlugin()
   ]
