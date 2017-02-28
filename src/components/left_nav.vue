a98c61f94165cd13caadbd04952bf98ffb85a085
ICELI <woticeli@gmail.com>
2017-02-28 14:23:26 +0800

    refactor: leftNav with vue

--- src/components/left_nav.vue
+++ src/components/left_nav.vue
@@ -6,191 +6,30 @@
       <input type="text" placeholder="搜索组件名..." _v-6db9881e="" class="search">
     </div>
     <div class="nav-item">
-      <h1>组件</h1>
-      <!-- general-->
-      <ul>
-        <li class="nav-group-title">General</li>
-        <li class="nav-group-list">
-          <router-link to="/component/button">Button 按钮</router-link>
-        </li>
-      </ul>
-      <!-- form-->
-      <ul>
-        <li class="nav-group-title">Form</li>
-        <li class="nav-group-list">
-          <a href="../../components/input/demo/input.html">
-            <span>Input</span>
-            <span>输入框</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/radio/demo/radio.html">
-            <span>Radio</span>
-            <span>单选框</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/checkbox/demo/checkbox.html">
-            <span>Checkbox</span>
-            <span>多选框</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/select/demo/select.html">
-            <span>Select</span>
-            <span>选择器</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/datetime/demo/time.html">
-            <span>TimePicker</span>
-            <span>时间选择器</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/datetime/demo/date.html">
-            <span>DatePicker</span>
-            <span>日期选择器</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/datetime/demo/index.html">
-            <span>DateTimePicker</span>
-            <span>日期时间选择器</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/upload/demo/upload.html">
-            <span>Upload</span>
-            <span>上传</span>
-          </a>
-        </li>
-      </ul>
+      <h1>{{navList.name}}</h1>
 
-      <!-- data-->
-      <ul>
-        <li class="nav-group-title">Data</li>
-        <li class="nav-group-list">
-          <a href="../../components/tag/demo/tag.html">
-            <span>Tag</span>
-            <span>标签</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/upload/demo/pagination.html">
-            <span>Pagination</span>
-            <span>分页</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/badge/demo/badge.html">
-            <span>Badge</span>
-            <span>标记</span>
-          </a>
-        </li>
-      </ul>
+      <!-- 分组 -->
+      <template v-if="navList.groups">
+        <ul v-for="group in navList.groups">
+          <li class="nav-group-title">{{group.groupName}}</li>
+          <li class="nav-group-list" v-for="navItem in group.list">
+            <router-link :to="'/component/'+navItem.path">{{navItem.title}}</router-link>
+          </li>
+        </ul>
+      </template>
 
-      <!-- notice-->
-      <ul>
-        <li class="nav-group-title">Notice</li>
-        <li class="nav-group-list">
-          <a href="../../components/alertWarning/demo/alert.html">
-            <span>Alert</span>
-            <span>警告</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/loading/demo/load.html">
-            <span>Loading</span>
-            <span>加载</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/message/demo/message.html">
-            <span>Message</span>
-            <span>消息提示</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/messagebox/demo/messagebox.html">
-            <span>MessageBox</span>
-            <span>弹框</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/notification/demo/notification.html">
-            <span>Notification</span>
-            <span>通知</span>
-          </a>
-        </li>
-      </ul>
-
-      <!-- Navigation-->
-      <ul>
-        <li class="nav-group-title">Navigation</li>
-        <li class="nav-group-list">
-          <a href="../../components/navmenu/demo/navmenu.html">
-            <span>NavMenu</span>
-            <span>导航菜单</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="">
-            <span>Tabs</span>
-            <span>标签页</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/breadCrumb/demo/breadCrumb.html">
-            <span>Breadcrumb</span>
-            <span>面包屑</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/dropdown/demo/dropdown.html">
-            <span>Dropdown</span>
-            <span>下拉菜单</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/steps/demo/steps.html">
-            <span>Steps</span>
-            <span>步骤</span>
-          </a>
-        </li>
-      </ul>
-      <!-- Others-->
-      <ul>
-        <li class="nav-group-title">Others</li>
-        <li class="nav-group-list">
-          <a href="../../components/dialog/demo/dialog.html">
-            <span>Dialog</span>
-            <span>对话框</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/tooltip/demo/tooltip.html">
-            <span>Tooltip</span>
-            <span>文字提示</span>
-          </a>
-        </li>
-        <li class="nav-group-list">
-          <a href="../../components/popover/demo/popover.html">
-            <span>Popover</span>
-            <span>弹出框</span>
-          </a>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
 
 <script>
+  import navList from '../../config/nav.config.json'
+
   export default {
     name: 'leftNav',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        navList
       }
     }
   }
