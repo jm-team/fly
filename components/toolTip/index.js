7d54aae38ccb0602693a9c0005aefb142fd5873b
luonadou <luonadou@sina.com>
2017-04-24 17:42:39 +0800

    tip ui

--- components/toolTip/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @authors Your Name (you@example.org)
- * @date    2017-02-13 14:21:46
- * @version $Id$
- */
- import ToolTip from './toolTip';
- console.log('ToolTip',ToolTip)
- export default ToolTip;
--- components/toolTip/style/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- * @authors Your Name (you@example.org)
- * @date    2017-02-13 14:21:46
- * @version $Id$
- */
- import './jquery.qtip.min.scss'
-
